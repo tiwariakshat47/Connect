3f9b00699cb932968d4fa24f497fc3385ef4e5a1
Ryan-L-Chapman <rlchapman555@gmail.com>
2024-07-28 18:57:04 -0700

    Dropdown in progress

--- ConnectApp/app/(tabs)/index.tsx
+++ ConnectApp/app/(tabs)/index.tsx
@@ -2,12 +2,29 @@ import { Pressable, StyleSheet } from "react-native";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import { useState } from "react";
+import { Dropdown } from "react-native-element-dropdown";
 
 export default function TabOneScreen() {
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
   const deleteClass = () => {
     //pass
   };
-  
+
+  //This is temporary placeholder data
+  const data = [
+    { label: "Item 1", value: "1" },
+    { label: "Item 2", value: "2" },
+    { label: "Item 3", value: "3" },
+    { label: "Item 4", value: "4" },
+    { label: "Item 5", value: "5" },
+    { label: "Item 6", value: "6" },
+    { label: "Item 7", value: "7" },
+    { label: "Item 8", value: "8" },
+  ];
+
   return (
     <View style={styles.container}>
       <Pressable onPress={deleteClass}>
@@ -22,6 +39,17 @@ export default function TabOneScreen() {
           Delete Class
         </Text>
       </Pressable>
+      <Dropdown
+        search
+        data={data}
+        labelField="label"
+        valueField="value"
+        onChange={item => {
+          setValue(item.value);
+          setIsFocus(false);
+        }}
+        searchPlaceholder="Search Class by Name, Code, or Professor..."
+      />
     </View>
   );
 }
